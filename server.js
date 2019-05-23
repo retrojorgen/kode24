135eb0e1fecde9ba69ca58bbf2aed8c8e2806046
Jørgen Jacobsen <jorgeja@gmail.com>
2019-05-23 11:30:58 +0200

    setinterval removed

--- server.js
+++ server.js
@@ -61,9 +61,3 @@ const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`listening on ${port}`);
-
-var http = require("http");
-
-setInterval(function() {
-  http.get("http://kode24-signup.herokuapp.com/");
-}, 300000); // every 5 minutes (300000)
