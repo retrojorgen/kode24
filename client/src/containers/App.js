ce5e0cc85c3acc796db3186c337b6b2180a28d9f
Jørgen Jacobsen <jorgeja@gmail.com>
2019-05-23 11:25:12 +0200

    inline form

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,14 +1,14 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Front from './Front';
-import Inline from './Inline';
+import React, { Component } from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+import Front from "./Front";
+import Inline from "./Inline";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter className="app">
         <div>
-          <Route exact path="/" component={Front} />
+          <Route exact path="/" component={Inline} />
           <Route path="/inline" component={Inline} />
         </div>
       </BrowserRouter>
--- server.js
+++ server.js
@@ -1,54 +1,60 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const Mailchimp = require('mailchimp-api-v3');
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const path = require("path");
+const bodyParser = require("body-parser");
+const Mailchimp = require("mailchimp-api-v3");
 const mailchimp = new Mailchimp(process.env.MAILCHIMP_KODE24);
+const cors = require("cors");
 
 const app = express();
 
+app.use(cors());
+
 // Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
- 
+
 // parse application/json
 app.use(bodyParser.json());
 
 // Put all API endpoints under '/api'
-app.get('/api/test', (req, res) => {
-  res.json({title: "kode24"});
+app.get("/api/test", (req, res) => {
+  res.json({ title: "kode24" });
 });
 
-app.post('/api/email', (req, res) => {
-
-
+app.post("/api/email", (req, res) => {
   const email = req.body.email;
   const name = req.body.name;
   const nameList = name.split(" ");
   const firstName = nameList[0];
-  const lastName = nameList.length > 1 ? nameList.splice(1, nameList.length).join(" ") : "";
+  const lastName =
+    nameList.length > 1 ? nameList.splice(1, nameList.length).join(" ") : "";
 
-  mailchimp.post("/lists/" + process.env.MAILCHIMP_KODE24_LIST_ID + "/members/",
-  {
-    "email_address": email,
-    "status": "subscribed",
-    "merge_fields": {
-        "FNAME": firstName,
-        "LNAME": lastName
-    }
-  })
-  .then(function(results) {
-    res.json(results);
-  })
-  .catch(function (err) {
-    res.json(err);
-  })
+  mailchimp
+    .post("/lists/" + process.env.MAILCHIMP_KODE24_LIST_ID + "/members/", {
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    })
+    .then(function(results) {
+      res.json(results);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
@@ -56,11 +62,8 @@ app.listen(port);
 
 console.log(`listening on ${port}`);
 
-
-
-
 var http = require("http");
 
 setInterval(function() {
-    http.get("http://kode24-signup.herokuapp.com/");
+  http.get("http://kode24-signup.herokuapp.com/");
 }, 300000); // every 5 minutes (300000)
