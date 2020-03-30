f1bcf885b4f2e475b3d51b592566321200b1805b
retrojorgen <jorgeja@gmail.com>
2020-03-30 20:23:29 +0200

    logs

--- server.js
+++ server.js
@@ -40,6 +40,7 @@ const getMembers = async (url, callback) => {
         return member.attributes.full_name;
     });
 
+    console.log(allData.length, members.length, 'data lengths');
     allData = allData.concat(members);
     if(data.data.links && data.data.links.next) {
         getMembers(data.data.links.next, callback);
@@ -55,6 +56,7 @@ const getMembers = async (url, callback) => {
 setInterval(() => { 
   // reset allData
   allData = [];
+  console.log(allData.length);
 
   getMembers(membersUrl, () => {}); 
 }, 60000);
